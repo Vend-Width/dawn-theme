caf457ee5e63323c8b5e85ae3666211c41e95164
Monting Lin <mtl.pub@gmail.com>
2024-02-08 19:50:16 +0800

    add remtopx plugin from blankbob https://www.youtube.com/watch?v=TWA3ZFiTvsY

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import remToPxPlugin from 'tailwindcss-rem-to-px';
+
 export default {
   content: ['./**/*.{liquid,json,js}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [remToPxPlugin({ baseFontSize: 16 })],
 };
